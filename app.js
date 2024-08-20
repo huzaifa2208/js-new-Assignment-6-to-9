d45c48c8554f1a0e2895ae63aedd8bb2020422ac
huzaifa2208 <www.hq03112119535@gmail>
2024-08-20 08:42:27 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,59 @@
+//////////////////QUESTION NO 1//////////////////////////////
+
+// var a = 15
+// var value = ++a + a++ + --a + a--
+
+// console.log ("result :"+ value)
+// console.log("the value of a is: 15")
+
+// console.log("the value of ++a is: 16")
+// console.log("Now the value of a is: 16")
+
+// console.log("the value of a++ is: 16")
+// console.log("Now the value of a is: 17")
+
+// console.log("the value of --a is: 16")
+// console.log("Now the value of a is: 16")
+
+// console.log("the value of a-- is: 16")
+// console.log("Now the value of a is: 15")
+
+
+//////////////////QUESTION NO 2//////////////////////////////
+
+// var a = 2
+// var b = 1
+// var value = --a; 
+// var value = --a - --b; //0
+// var value = --a - --b + ++b; // 1
+// var value = --a - --b + ++b + b--; -0
+//                                             /////////A is 1
+//                                             ////////B is -0
+//                                             ///////Result -1
+// console.log(value)                 
+
+//////////////////QUESTION NO 3/////////////////////////////
+
+// var userName = +prompt ("please enter your name")
+// var massage = "hello , " + userName + " !well come"
+// console.log (massage)
+
+////////////////QUESTION NO 5//////////////////////////////
+
+
+// var num = +prompt("enter a number")
+// for (var i =1;i<=10;i++){
+//     console.log(num + "x " + i + "=" + num*i)
+// }
+
+// var num = prompt("Enter Number", "0") //prompt user to enter the number
+// var num = parseInt(num); //parse the num to number
+// var i = 0;
+// console.log('<table border="1" cellspacing="0">');
+// for (i = 1; i < 10; i++) {
+//   console.log("<tr><td>" + num + " x " + i + " = " + num * i + "</td></tr>");
+// }
+
+
+////////////////////////QUESTION NO 6///////////////////
+
